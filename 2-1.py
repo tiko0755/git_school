1e6d78a8f9d2f791798fa3cf72aa521545f64ff7
tiko0755 <zhy_sch@163.com>
2026-04-27 19:00:17 +0800

    the lesson 1

---
+++ 2-1.py
@@ -0,0 +1,10 @@
+import turtle               # 导入小海GUI模块
+
+print("Hello, world. 这是我的第一个程序, 请多指教.")
+
+turtle.pensize(5)           # 设置笔迹大小
+turtle.pencolor("red")      
+turtle.circle(50)
+
+print("程序运行完毕.")
+
